35b6eb5a4a502aba65096886745ebfa1e698e7a1
Maxwell Barvian <max@barvian.me>
2014-08-05 15:39:04 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-slider
-======
+# Slider
 
-A jQuery slider built around @thebird's Swipe plugin.
+A jQuery slider built around Brad Birdsall's [Swipe plugin](https://github.com/thebird/Swipe).
+
+## Installation
+
+Install via [Bower](http://bower.io):
+```
+$ bower install websolutions/slider --save
+```
+
+## Usage
+
+The most basic example follows this DOM structure:
+``` html
+<div class="slider">
+  <div class="slider-wrap">
+    <div>Slide #1</div>
+    <div>Slide #2</div>
+    <div>Slide #3</div>
+  </div>
+</div>
+```
+
+And is initialized like so:
+``` javascript
+$(".slider").slider();
+```
+
+Sliders can be destroyed at any time:
+``` javascript
+$(".slider").unslider();
+```
+
+The plugin also supports the Swipe's other documented [API methods](https://github.com/thebird/Swipe#swipe-api).
+
+### Configuring
+
+In addition to Swipe's [options](https://github.com/thebird/Swipe#config-options), the plugin can be configured as such:
+
+Option                      | Type     | Description                                                          | Default
+----------------------------|----------|----------------------------------------------------------------------|--------
+`arrows`                    | Boolean  | If the slider should use previous/next arrows                        | `true`
+`prevArrow`                 | String   | Markup for the previous arrow                                        | `<button type="button" class="slider-prev">Previous</button>`
+`nextArrow    `             | String   | Markup for the next arrow                                            | `<button type="button" class="slider-next">Next</button>`
+`disabledArrowClass`        | String   | Class name to apply to disabled arrows                               | `disabled`
+`paginated`                 | Boolean  | If the slider should use pagination                                  | `false`
+`paginationClass`           | String   | Class name to apply to the pagination container                      | `slider-pagination`
+`customPage`                | Function | Function to build pagination items                                   |
+`currentPageClass`          | String   | Class name to apply to the current pagination item                   | `current`
---
+++ js/wsol.slider.js
@@ -0,0 +1,211 @@
+/**
+ * wsol.slider.js 1.0.0
+ * http://github.com/websolutions/slider
+ */
+
+
+;(function ($, window, document, undefined) {
+
+  var defaults = {
+    // Swipe config
+    startSlide: 0,
+    speed: 300,
+    auto: 0,
+    continuous: true,
+    disableScroll: false,
+    stopPropagation: false,
+    callback: null,
+    transitionEnd: null,
+
+    // Custom config
+    arrows: true,
+    prevArrow: '<button type="button" class="slider-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slider-next">Next</button>',
+    disabledArrowClass: 'disabled',
+    paginated: false,
+    paginationClass: "slider-pagination",
+    customPage: function(slider, index) {
+      return '<button type="button">' + (index + 1) + '</button>';
+    },
+    currentPageClass: 'current'
+  };
+
+  function Slider(element, options) {
+    this.$slider = $(element);
+
+    this.changeSlide = $.proxy(this.changeSlide, this);
+    this.slideHandler = $.proxy(this.slideHandler, this);
+
+    this.settings = $.extend({}, defaults, options);
+    this.swipe = new Swipe(element, $.extend({}, this.settings, {
+      callback: this.slideHandler,
+      transitionEnd: this.settings.transitionEnd ? this.settings.transitionEnd.bind(this, this) : null
+    }));
+
+    this.init();
+  }
+
+  Slider.prototype.init = function() {
+    if (this.settings.arrows) {
+      this.buildArrows();
+      this.updateArrows();
+
+      this.$prevArrow.on("click.slider", {
+        message: "previous"
+      }, this.changeSlide);
+      this.$nextArrow.on("click.slider", {
+        message: "next"
+      }, this.changeSlide);
+    }
+    if (this.settings.paginated) {
+      this.buildPagination();
+      this.updatePagination();
+
+      this.$pagination.children("li").on("click.slider", {
+        message: "index"
+      }, this.changeSlide);
+    }
+  };
+
+  Slider.prototype.buildArrows = function() {
+    this.$prevArrow = $(this.settings.prevArrow).appendTo(this.$slider);
+    this.$nextArrow = $(this.settings.nextArrow).appendTo(this.$slider);
+  };
+
+  Slider.prototype.buildPagination = function() {
+    var paginationString = '<ol class="' + this.settings.paginationClass + '">';
+    for (var i = 0, l = this.swipe.getNumSlides(); i < l; i++) {
+      paginationString += '<li>' + this.settings.customPage.call(this, this, i) + '</li>';
+    }
+    paginationString += '</ol>';
+
+    this.$pagination = $(paginationString).appendTo(this.$slider);
+  };
+
+  Slider.prototype.updateArrows = function() {
+    var current = this.swipe.getPos(),
+        length = this.swipe.getNumSlides();
+
+    if (!this.settings.continuous) {
+      if (0 < current) {
+        this.$prevArrow.removeClass(this.settings.disabledArrowClass);
+        if (this.$prevArrow.is("input") || this.$prevArrow.is("button")) this.$prevArrow.removeAttr("disabled");
+      } else {
+        this.$prevArrow.addClass(this.settings.disabledArrowClass);
+        if (this.$prevArrow.is("input") || this.$prevArrow.is("button")) this.$prevArrow.attr("disabled", "disabled");
+      }
+
+      if (current + 1 < length) {
+        this.$nextArrow.removeClass(this.settings.disabledArrowClass);
+        if (this.$nextArrow.is("input") || this.$nextArrow.is("button")) this.$nextArrow.removeAttr("disabled");
+      } else {
+        this.$nextArrow.addClass(this.settings.disabledArrowClass);
+        if (this.$nextArrow.is("input") || this.$nextArrow.is("button")) this.$nextArrow.attr("disabled", "disabled");
+      }
+    }
+  };
+
+  Slider.prototype.updatePagination = function() {
+    var current = this.swipe.getPos();
+
+    this.$pagination.children("li")
+      .removeClass(this.settings.currentPageClass)
+      .eq(current).addClass(this.settings.currentPageClass);
+  };
+
+  Slider.prototype.changeSlide = function(event) {
+    var $target = $(event.target);
+
+    // Prevent default action if target is a link
+    $target.is('a') && event.preventDefault();
+
+    switch(event.data.message) {
+      case 'previous':
+        this.swipe.prev();
+        break;
+
+      case 'next':
+        this.swipe.next();
+        break;
+
+      case 'index':
+        this.swipe.slide($target.closest("li").index());
+        break;
+    }
+  };
+
+  Slider.prototype.slideHandler = function(index, slide) {
+    if (this.settings.arrows) {
+      this.updateArrows();
+    }
+    if (this.settings.paginated) {
+      this.updatePagination();
+    }
+
+    if (this.settings.callback != null) {
+      this.settings.callback.call(this, this, index, slide);
+    }
+  };
+
+  Slider.prototype.destroy = function() {
+    this.swipe.kill();
+
+    if (this.settings.arrows) {
+      this.$prevArrow.remove();
+      this.$nextArrow.remove();
+    }
+    if (this.settings.paginated) {
+      this.$pagination.remove()
+    }
+  };
+
+  // Delete existing Swipe jQuery plugin
+  delete $.fn.Swipe;
+
+  $.fn.slider = function(options) {
+    return this.each(function(index, element) {
+      element.slider = new Slider(element, options);
+    });
+  };
+
+  $.fn.sliderPrev = function() {
+    return this.each(function(index, element) {
+      if (element.slider) {
+        element.slider.swipe.prev();
+      }
+    });
+  };
+
+  $.fn.sliderNext = function() {
+    return this.each(function(index, element) {
+      if (element.slider) {
+        element.slider.swipe.next();
+      }
+    });
+  };
+
+  $.fn.sliderGetPos = function() {
+    return this.get(0).slider.swipe.getPos();
+  };
+
+  $.fn.sliderGetNumSlides = function() {
+    return this.get(0).slider.swipe.getNumSlides();
+  };
+
+  $.fn.sliderSlide = function(slide, duration) {
+    return this.each(function(index, element) {
+      if (element.slider) {
+        element.slider.swipe.slide(slide, duration);
+      }
+    });
+  };
+
+  $.fn.unslider = function() {
+    return this.each(function(index, element) {
+      if (element.slider) {
+        element.slider.destroy();
+      }
+    });
+  };
+
+})(jQuery, window, document);
